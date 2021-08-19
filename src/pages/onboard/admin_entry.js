60e3c74a9af6d735d8e71633a372233057ec9fe5
pariola-droid <kehindephilip15@gmail.com>
2021-08-19 18:46:42 +0100

    added the deleted lines back

---
+++ src/pages/onboard/admin_entry.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react'
+import {callApi, e, swr, apiConfigs, url} from '../../adapter/common'
+import { Link } from 'react-router-dom'
+
+import InputField from '../../components/inputField'
+import Button from '../../components/button'
+
+import check from "../../assets/images/check.svg";
+
+import styles from "../../styles/onboard/sign_in.module.scss";
+
+class AdminEntry extends Component{
+    
+    constructor (props){
+        super(props)
+
+        this.state = {
+            email: '',
+            password: '',
+            isLoading: false
+        }
+    }
+    componentDidMount(){
+        if(localStorage.user){
+            localStorage.removeItem('user')
+        }
+    }
+    model(e){
+        this.setState({
+            [e.name]: e.text
+        })
+    }
+
+    async login(){
+        if(this.state.isLoading === true){
+            return
+        }
+        
+        if(this.state.email.trim()==='' || this.state.password.trim()===''){
+            return e("Please, ensure you have filled in all fields")
+        }
+        else{
+            this.setState({
+                isLoading : true
+            })
+
+            const res = await callApi('post', apiConfigs.apiUrl+'ApplicationUser/admin.cs', this.state)
+            if(res.status === 200){
+                localStorage.setItem('user', JSON.stringify(res.data))
+                this.props.history.push('/dashboard/admin/home')
+            }
+            else{
+                swr(res.data.message)
+            }
+
+            this.setState({
+                isLoading : false
+            })
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <section className={styles.sign_in}>
+                <div className={styles.info}>
+                    <div className={styles.info_nav}>
+                        <Link to="/">back</Link>
+                    </div>
+                    <div className={styles.info_content}>
+                        <div className={styles.head}>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z" />
+                            </svg>
+                            <h6>1Stash</h6>
+                            <p>free</p>
+                        </div>
+
+                        <div className={styles.features}>
+                            <img src={check} alt="" />
+                            <div className={styles.text}>
+                                <h3>Join 3,200+ Designers</h3>
+                                <p>Connect with over 3,200+ UX/UI designers</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.features}>
+                            <img src={check} alt="" />
+                            <div className={styles.text}>
+                                <h3>Join 3,200+ Designers</h3>
+                                <p>Connect with over 3,200+ UX/UI designers</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.features}>
+                            <img src={check} alt="" />
+                            <div className={styles.text}>
+                                <h3>Join 3,200+ Designers</h3>
+                                <p>Connect with over 3,200+ UX/UI designers</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.features}>
+                            <img src={check} alt="" />
+                            <div className={styles.text}>
+                                <h3>Join 3,200+ Designers</h3>
+                                <p>Connect with over 3,200+ UX/UI designers</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div className={styles.form}>
+                    <div className={styles.form_nav}>
+                        <Link to="/">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z" />
+                            </svg>
+                        </Link>                        
+
+                        <Link to={url.signIn}>
+                            User entry
+                        </Link>
+                    </div>
+
+                    <h5 className={styles.h5}>
+                        Login as an admin!
+                    </h5>
+
+                    <div className={styles.form_ic}>
+
+                        <InputField type="email" placeholder="Email address" placeinside="Email address"  name="email" inputValue={e => this.model(e)} />
+                            
+                        <InputField type="password" name="password" placeholder="Password" placeinside="Password" inputValue={e => this.model(e)} />
+                        
+                        <Button onClick={() => this.login()} isLoading={this.state.isLoading} text="SIGN IN"/>
+                        
+                    </div>
+
+                    <div className={styles.terms}>
+                        <a href="#0" target="_blank" class="tiny-link">Forgot Password?</a>.
+                    </div>
+                </div>
+            </section>
+        </div>
+        )
+    }
+}
+     
+
+export default AdminEntry
